3c484e8bc61bf583ddd984cb83aa81edd2d31e59
Gabriel Gonzalez <Gabriel439@gmail.com>
2013-03-30 13:43:01 -0700

    Reorganized implementation

--- Control/Proxy/STM.hs
+++ Control/Proxy/STM.hs
@@ -1,17 +1,19 @@
--- | Actor-style communication between concurrent pipelines
-
-{-# LANGUAGE PolymorphicComponents #-}
+-- | Asynchronous communication between concurrent pipelines
 
 module Control.Proxy.STM (
-    -- * Common API
-    Process(..),
-    Mailbox(..),
-    sendD,
+    -- * Spawn Inputs and Outputs
+    spawn,
+    Buffer(..),
+    Input,
+    Output,
 
-    -- * Implementations
-    spawnBounded,
-    spawnUnbounded,
-    spawnSingle
+    -- * Send and receive messages
+    send,
+    recv,
+
+    -- * Proxy utilities
+    sendD,
+    runS
     ) where
 
 import Control.Applicative ((<|>), (<*), pure)
@@ -20,28 +22,41 @@ import qualified Control.Concurrent.STM as S
 import qualified Control.Proxy as P
 import Data.IORef (newIORef, readIORef, mkWeakIORef, IORef)
 
--- | A 'Process' produces all values sent to its associated 'Mailbox'
-newtype Process a = Process
-    { run :: forall p . (P.Proxy p) => () -> P.Producer p a IO () }
-
--- | 'send' values to a 'Mailbox' to deliver them to the corresponding 'Process'
-newtype Mailbox a = Mailbox { send :: a -> IO Bool }
-
--- | Writes all values flowing \'@D@\'ownstream to the given 'Mailbox'
-sendD :: (P.Proxy p) => Mailbox a -> x -> p x a x a IO ()
-sendD mailbox = P.runIdentityK loop
-  where
-    loop x = do
-        a <- P.request x
-        done <- lift $ send mailbox a
-        if done
-            then return ()
-            else do
-                x2 <- P.respond a
-                loop x2
+{-| Spawn an 'Input' \/ 'Output' pair that communicate using the specified
+    'Buffer'
+-}
+spawn :: Buffer -> IO (Input a, Output a)
+spawn buffer = spawnWith $ case buffer of
+    Bounded n -> do
+        q <- S.newTBQueueIO n
+        let read = do
+                ma <- S.readTBQueue q
+                case ma of
+                    Nothing -> S.unGetTBQueue q ma
+                    _       -> return ()
+                return ma
+        return (read, S.writeTBQueue q)
+    Unbounded -> do
+        q <- S.newTQueueIO
+        let read = do
+                ma <- S.readTQueue q
+                case ma of
+                    Nothing -> S.unGetTQueue q ma
+                    _       -> return ()
+                return ma
+        return (read, S.writeTQueue q)
+    Single    -> do
+        m <- S.newEmptyTMVarIO
+        let read = do
+                ma <- S.takeTMVar m
+                case ma of
+                    Nothing -> S.putTMVar m ma
+                    _       -> return ()
+                return ma
+        return (read, S.putTMVar m)
 
 spawnWith
-    :: IO (S.STM (Maybe a), Maybe a -> S.STM ()) -> IO (Mailbox a, Process a)
+    :: IO (S.STM (Maybe a), Maybe a -> S.STM ()) -> IO (Input a, Output a)
 spawnWith create = do
     (read, write) <- create
     rUp  <- newIORef ()  -- Keep track of whether upstream pipes are still live
@@ -52,47 +67,70 @@ spawnWith create = do
     let quit = do
             b <- S.readTVar done
             S.check b
-            return True
+            return False
         continue a = do
             write (Just a)
-            return False
+            return True
         _send a = S.atomically (quit <|> continue a) <* readIORef rUp
-    return (Mailbox _send , Process (sourceWith read rDn))
+        _recv = S.atomically read <* readIORef rDn
+    return (Input _send , Output _recv)
 
-sourceWith
-    :: (P.Proxy p) => S.STM (Maybe a) -> IORef () -> () -> P.Producer p a IO ()
-sourceWith read rDn () = P.runIdentityP go
+{-| 'Buffer' specifies how many messages to buffer between the 'Input' and
+    'Output' before 'send' blocks.
+-}
+data Buffer
+    -- | Store an unlimited number of messages
+    = Unbounded
+    -- | Store a finite number of messages specified by the 'Int' argument
+    | Bounded Int
+    -- | Store only a single message (like @Bounded 1@, but more efficient)
+    | Single
+
+-- | Receives messages for the associated 'Output'
+newtype Input a = Input {
+    {-| Send a message to the 'Input' end, blocking if the 'Buffer' is full
+
+        Returns 'False' if the associated 'Output' is garbage collected, 'True'
+        otherwise
+    -}
+    send :: a -> IO Bool }
+
+-- | Produces all messages sent to the associated 'Input'
+newtype Output a = Output {
+    {-| Receive a message from the 'Output' end, blocking while the 'Buffer' is
+        empty
+
+        Returns 'Nothing' if the 'Input' end has been garbage collected
+    -}
+    recv :: IO (Maybe a) }
+
+{-| Writes all messages flowing \'@D@\'ownstream to the given 'Input'
+
+    'sendD' terminates when the corresponding 'Output' is garbage collected.
+-}
+sendD :: (P.Proxy p) => Input a -> x -> p x a x a IO ()
+sendD mailbox = P.runIdentityK loop
+  where
+    loop x = do
+        a <- P.request x
+        alive <- lift $ send mailbox a
+        if alive
+            then do
+                x2 <- P.respond a
+                loop x2
+            else return ()
+
+{-| Convert an 'Output' to a 'P.Producer'
+
+    'runS' terminates when the corresponding 'Input' is garbage collected.
+-}
+runS :: (P.Proxy p) => Output a -> () -> P.Producer p a IO ()
+runS process () = P.runIdentityP go
   where
     go = do
-        ma <- lift $ S.atomically read
-        lift $ readIORef rDn
+        ma <- lift $ recv process
         case ma of
             Nothing -> return ()
             Just a  -> do
                 P.respond a
                 go
-
-{-| Spawn an associated 'Mailbox' and 'Process' that communicate using a
-    'TBQueue'
-
-    The 'Int' parameter specifies the size of the 'TBQueue'
--}
-spawnBounded :: Int -> IO (Mailbox a, Process a)
-spawnBounded n = spawnWith $ do
-    q <- S.newTBQueueIO n
-    return (S.readTBQueue q, S.writeTBQueue q)
-
-{-| Spawn an associated 'Mailbox' and 'Process' that communicate using a
-    'TQueue'
--}
-spawnUnbounded :: IO (Mailbox a, Process a)
-spawnUnbounded = spawnWith $ do
-    q <- S.newTQueueIO
-    return (S.readTQueue q, S.writeTQueue q)
-
-{-| Spawn an associated 'Mailbox' and 'Process' that communicate using a 'TMVar'
--}
-spawnSingle :: IO (Mailbox a, Process a)
-spawnSingle = spawnWith $ do
-    m <- S.newEmptyTMVarIO
-    return (S.takeTMVar m, S.putTMVar m)
